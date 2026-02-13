476a94aa128301018b62cd58c0525fa214dd7edb
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-02-13 14:17:26 +0545

    six: waving hand animation

--- src/components/WavingHand.jsx
+++ src/components/WavingHand.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const wave = keyframes`
+from {
+transform: rotate(-10deg);
+}
+to {
+transform: rotate(30deg);
+}
+`;
 
 const Wave = styled.span`
   font-size: 3rem;
+  display: inline-block;
+  animation: ${wave} 1000ms infinite alternate ease-in-out;
+  transform-origin: 75% 80%;
 `;
 
 export default function WavingHand() {
